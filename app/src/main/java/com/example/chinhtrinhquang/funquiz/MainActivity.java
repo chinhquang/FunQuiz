b560ba2859b44adce3882ddc8174c1f325cf69f3
Trình Quang Chính <trinhquangchinh171298@gmail.com>
2018-11-14 12:44:29 +0700

    Initial view

---
+++ app/src/main/java/com/example/chinhtrinhquang/funquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chinhtrinhquang.funquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
