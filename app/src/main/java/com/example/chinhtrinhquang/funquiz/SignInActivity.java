92b2cce70a69f81d9b637b59f7736d37eccc4c55
chinhquang <trinhquangchinh171298@gmail.com>
2018-12-31 11:26:02 +0700

    Save password

--- app/src/main/java/com/example/chinhtrinhquang/funquiz/SignInActivity.java
+++ app/src/main/java/com/example/chinhtrinhquang/funquiz/SignInActivity.java
@@ -2,11 +2,13 @@ package com.example.chinhtrinhquang.funquiz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,11 +22,18 @@ public class SignInActivity extends AppCompatActivity {
     Button btnSignIn;
     Button btnBack;
     Database database;
+    CheckBox checkBoxNhoMatKhau;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+
         Inflate();
+        sharedPreferences = getSharedPreferences("dataLogin",MODE_PRIVATE);
+        txtPassword.setText(sharedPreferences.getString("matkhau",""));
+        txtUsername.setText(sharedPreferences.getString("taikhoan",""));
+        checkBoxNhoMatKhau.setChecked(sharedPreferences.getBoolean("checked",false));
         String DB_Path = "/data/data/com.example.chinhtrinhquang.funquiz/databases/";
         String Database_Name = "xxx.sqlite";
         database = new Database(this,Database_Name,null,1,DB_Path);
@@ -45,6 +54,19 @@ public class SignInActivity extends AppCompatActivity {
                         SignInActivity.username = us;
                         Toast.makeText(getContext(),"Loggin successfully",Toast.LENGTH_SHORT).show();
                         data.close();
+                        if (checkBoxNhoMatKhau.isChecked()){
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("taikhoan",us);
+                            editor.putString("matkhau",pw);
+                            editor.putBoolean("checked",true);
+                            editor.commit();
+                        }else{
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.remove("taikhoan");
+                            editor.remove("matkhau");
+                            editor.remove("checked");
+                            editor.commit();
+                        }
 
                         Intent intent = new Intent(getApplication(), OptionsActivity.class);
                         startActivity(intent);
@@ -74,6 +96,7 @@ public class SignInActivity extends AppCompatActivity {
         txtPassword = (EditText)findViewById(R.id.txtPassword);
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
         btnBack = (Button) findViewById(R.id.btnBack);
+        checkBoxNhoMatKhau = (CheckBox) findViewById(R.id.cbNhoMatKhau);
     }
 
 }
